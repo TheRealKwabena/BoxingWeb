a8f228b3d11dc6b464749ebc90a154facda423db
Kwabena Asare <kwabenaa@net.chalmmers.se>
2023-01-29 16:58:59 +0100

    feat: footer design started

--- boxing_ui/src/components/Footer/Elements.ts
+++ boxing_ui/src/components/Footer/Elements.ts
@@ -7,7 +7,7 @@ export const Body = styled.div`
     align-items: center;
     background-color: #000000;
     color: #FFFFFF;
-    position: absolute;
+    position: relative;
     width: 100%;
     min-height: 15vw;
     bottom: 0
--- boxing_ui/src/components/Footer/Footer.tsx
+++ boxing_ui/src/components/Footer/Footer.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import {Body} from './Elements'
+import './Footer.css'
 const Footer = () => {
   return (
     <Body>
-      <p>Footer</p>
+      <div className='text-container'>
+        <h3 className='text'>Ready for your <span style={{color: '#C90B0B'}}>next</span> lesson</h3>
+      </div>
     </Body>
   )
 }

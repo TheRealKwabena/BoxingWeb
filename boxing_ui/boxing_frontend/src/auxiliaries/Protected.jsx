c34603bb6c7b442078e034c2bc967aae3ba439f4
TheRealKwabena <gusasarkw@student.gu.se>
2023-05-27 01:43:53 +0200

    feat: API route for login connected but still in progress for testing

--- boxing_ui/boxing_frontend/src/auxiliaries/Protected.jsx
+++ boxing_ui/boxing_frontend/src/auxiliaries/Protected.jsx
@@ -3,7 +3,7 @@ import {Navigate} from 'react-router-dom'
 
 const Protected = ({loggedIn, children}) => {
     if(!loggedIn) {
-        return <Navigate to="/login" replace />
+        return <Navigate to="/" replace />
     }
     else {
         return children
--- boxing_ui/boxing_frontend/src/components/Header/Header.tsx
+++ boxing_ui/boxing_frontend/src/components/Header/Header.tsx
@@ -3,7 +3,8 @@ import Logo from '../../assets/glovesicon.png'
 import {Nav, LogoContainer, NavMenu, NavLink, SignInButton } from './HeaderElements'
 import {Link} from 'react-router-dom'
 import './Header.css'
-const Header = () => {
+import { ILogoutProps } from '../../pages/HomePage'
+const Header:React.FC<ILogoutProps> = ({logoutUser}) => {
   return (
    <>
    <Nav>
@@ -13,11 +14,13 @@ const Header = () => {
     </LogoContainer>
     <NavMenu>
         <ul id='navbar'>
-            <li><Link to='/'>HOME</Link></li>
-            <li><Link to='/'>ABOUT</Link></li>
-            <li><Link to='/'>LOCATION</Link></li>
-            <li><Link to='/'>CONTACT</Link></li>
-            <li><Link to='/register'><SignInButton className='btn'>SIGN UP</SignInButton></Link></li>
+            <li><Link to='/home'>HOME</Link></li>
+            <li><Link to='/home'>ABOUT</Link></li>
+            <li><Link to='/home'>LOCATION</Link></li>
+            <li><Link to='/home'>CONTACT</Link></li>
+            <li><Link to='/' onClick={
+                logoutUser
+            }><SignInButton className='btn'>SIGN OUT</SignInButton></Link></li>
         </ul>
     </NavMenu>
     
